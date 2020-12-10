df88509a6a61ea73f5e019dfa2e64228f855ac01
wanu7t <71253025+wanu7t@users.noreply.github.com>
2020-12-10 15:11:32 +0700

    Add files via upload

--- template/assets/js/login.js
+++ template/assets/js/login.js
@@ -1,15 +1,4 @@
 const input = document.querySelectorAll('.login_input');
-const progressBar = document.querySelector('.progress_line_work');
-
-input.forEach(inp =>{
-    inp.addEventListener('change',function(){
-        if(input[0].value.length > 0 || input[1].value.length > 0){
-            progressBar.classList.add('active');
-        }else{
-            progressBar.classList.remove('active');
-        }
-    })
-})
 input.forEach(i =>{
     i.addEventListener('focus',function(){
         inputParent = i.parentNode
