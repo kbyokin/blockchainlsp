2f49649bbe77b942e17b68f2ff17faaae25d94e2
kbyokin <kbyokin@gmail.com>
2020-12-16 22:49:33 +0900

    reorganize folder

--- template/apps/register-users.js
+++ template/apps/register-users.js
@@ -1,23 +1,44 @@
-const { get } = require('http');
 const https = require('https')
-
-var loginBtn = document.querySelector('#sign_btn');
 
 function RegisterUser() {
   // TODO: Recieved 2 variable from login.html
   var username = document.getElementById('username').value;
-  var password = document.getElementById("password").value;
+  var orgName = document.getElementById('orgName').value;
 
   // construct information from login.html to send to server
   const data = JSON.stringify({
     'username': username,
-    'password': password
+    'orgName': orgName
   })
-
   console.log("data:", data);
 
+  const options = {
+    hostname: 'localhost',
+    port: 4000,
+    path: '/users',
+    header: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+    }
+  }
+
+  const req = https.request(options, (res) => {
+    console.log('statusCode: ${res.statusCode}')
+
+    res.on('data', (d) => {
+      process.stdout.write(d)
+    })
+  })
+
+  req.on('error', (error) => {
+    console.error(error)
+  })
+  
+  req.write(data)
+  req.end()
+
   if (res.success == true) {
-    window.location = "accesstoken.html"; // redirect user to access token page
+    window.location = 'accesstoken.html'; // redirect user to access token page
   } else {
     alert("Username or Password are incorrect!")
   }
