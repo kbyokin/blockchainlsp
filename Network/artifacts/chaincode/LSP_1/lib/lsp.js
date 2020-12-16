8526049396a08675491084063e81106a427ab7c5
plumekakna <plumekakna@gmail.com>
2020-12-16 23:01:29 +0700

    update

--- Network/artifacts/chaincode/LSP_1/lib/lsp.js
+++ Network/artifacts/chaincode/LSP_1/lib/lsp.js
@@ -10,7 +10,7 @@ const { Contract } = require('fabric-contract-api');
 
 class LSP extends Contract {
 
-
+    // query order by ID
     async queryTransaction(ctx, orderID) {
         const orderAsBtyes = await ctx.stub.getState(orderID); // get the car from chaincode state
         if (!orderAsBtyes || orderAsBtyes.length === 0) {
@@ -22,7 +22,7 @@ class LSP extends Contract {
 
     // create order details (invoke by peer0 => customer services)
     async createTransaction(ctx, 
-        orderID, 
+        // orderID, 
         cargoOwner,
         loadingPoint, 
         loadingDateTime, 
@@ -36,7 +36,17 @@ class LSP extends Contract {
         console.info('============= START : Create Transaction Order Info ===========');
 
         let result;
+        let orderID = 'ORDER' + Math.random().toString(36).substr(2, 9);
+        let orderIDcheck;
         
+        while (true) {
+            orderIDcheck = await ctx.stub.getState(orderID); 
+            if (!orderIDcheck || orderIDcheck.length === 0) {
+                break;
+            }
+            orderID = 'ORDER' + Math.random().toString(36).substr(2, 9);
+        }
+
         if (peer == "peer0") {
             const order = {
                 orderID,
@@ -54,10 +64,11 @@ class LSP extends Contract {
             await ctx.stub.putState(orderID, Buffer.from(JSON.stringify(order)));
             console.info('============= END : Create Transaction Order Info ===========');
             result = true;
+            return [result, orderID];
         } else {
             result = false;
+            return result;
         }
-        return result;
         
     }
 
@@ -80,19 +91,54 @@ class LSP extends Contract {
         return JSON.stringify(allResults);
     }
 
-    async changeCargoOwner(ctx, orderID, newOwner, peer) {
-        console.info('============= START : changeCargoOwner ===========');
+    // async changeDataTransaction(ctx, 
+    //     orderID, 
+    //     cargoOwner,
+    //     loadingPoint, 
+    //     loadingDateTime, 
+    //     deliveryPoint,
+    //     deliveryDateTime, 
+    //     productID, 
+    //     quantity, 
+    //     packingDim, 
+    //     totalWeight,
+    //     peer) {
+    //     console.info('============= START : changeCargoOwner ===========');
 
-        const orderAsBtyes = await ctx.stub.getState(orderID); // get the transaction from chaincode state
-        if (!orderAsBtyes || orderAsBtyes.length === 0) {
-            throw new Error(`${orderID} does not exist`);
-        }
-        const order = JSON.parse(orderAsBtyes.toString());
-        order.cargoOwner = newOwner;
+    //     let result;
+        
+    //     if (peer == "peer0") {
+    //         const order = {
+    //             orderID,
+    //             cargoOwner,
+    //             loadingPoint,
+    //             loadingDateTime,
+    //             deliveryPoint,
+    //             deliveryDateTime,
+    //             productID,
+    //             quantity,
+    //             packingDim,
+    //             totalWeight,
+    //         };
+    
+    //         await ctx.stub.putState(orderID, Buffer.from(JSON.stringify(order)));
+    //         console.info('============= END : Create Transaction Order Info ===========');
+    //         result = true;
+    //     } else {
+    //         result = false;
+    //     }
+    //     return result;
 
-        await ctx.stub.putState(orderID, Buffer.from(JSON.stringify(order)));
-        console.info('============= END : changeCargoOwner ===========');
-    }
+    //     // const orderAsBtyes = await ctx.stub.getState(orderID); // get the transaction from chaincode state
+    //     // if (!orderAsBtyes || orderAsBtyes.length === 0) {
+    //     //     throw new Error(`${orderID} does not exist`);
+    //     // }
+    //     // const order = JSON.parse(orderAsBtyes.toString());
+    //     // order.cargoOwner = newOwner;
+
+    //     // await ctx.stub.putState(orderID, Buffer.from(JSON.stringify(order)));
+    //     // console.info('============= END : changeCargoOwner ===========');
+    // }
 
 }
 
