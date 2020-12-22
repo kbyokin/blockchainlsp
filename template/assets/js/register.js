f64acc4584685547242571f08080e9885382dc71
wanu7t <71253025+wanu7t@users.noreply.github.com>
2020-12-22 21:45:11 +0700

    fix register page

---
+++ template/assets/js/register.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#password').keyup(function(){
+        var valid = true
+        if(this.value.length >= 6){
+            $('.pattern_xix').children('.min6char').addClass('valid')
+        }else{
+            $('.pattern_xix').children('.min6char').removeClass('valid')
+        }
+
+        if(check_validate_password(this.value)){
+            $('.pattern_xix').children('.checkchar').addClass('valid')
+        }else{
+            $('.pattern_xix').children('.checkchar').removeClass('valid')
+        }
+    })
+
+    function check_validate_password(value){
+        var regEx = /^(?=.+[0-9])(?=.+[a-zA-Z])[a-zA-Z0-9]+$/;
+        var regTest = regEx.test(value);
+        return regTest;
+    }
+})
